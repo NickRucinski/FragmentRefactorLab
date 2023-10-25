8603b602b947b0daa06bdcb5a2156b20e2b78048
Nick Rucinski <nrucinski01@gmail.com>
2023-10-25 15:31:52 -0400

    created a fragment with the same views as the main layout

---
+++ app/src/main/java/edu/temple/fragmentrefactor/NameChangeFragment.kt
@@ -0,0 +1,22 @@
+package edu.temple.fragmentrefactor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class NameChangeFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_name_change, container, false)
+    }
+
+}
